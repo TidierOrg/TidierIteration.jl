3b1ce79fa2f951f943de9dfd57deb143a55b4314
G. Vituri <56522687+vituri@users.noreply.github.com>
2024-04-29 13:02:22 -0300

    try vitepress

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,35 @@
-using Documenter #, DocumenterVitepress
+using Documenter, DocumenterVitepress
 using TidierIteration
+
+fmt  = DocumenterVitepress.MarkdownVitepress(
+    repo = "https://github.com/TidierOrg/TidierIteration.jl",
+    devurl = "dev",
+    # deploy_url = "yourgithubusername.github.io/Tidier.jl.jl",
+)
+
+# makedocs(;
+#     modules = [Tidier],
+#     authors = "Karandeep Singh et al.",
+#     repo = "https://github.com/TidierOrg/Tidier.jl",
+#     sitename = "Tidier.jl",
+#     format = fmt,
+#     pages= pgs,
+#     warnonly = true,
+# )
+
+# deploydocs(;
+#     repo = "https://github.com/TidierOrg/Tidier.jl",
+#     target="build", # this is where Vitepress stores its output
+#     branch = "gh-pages",
+#     devbranch = "main",
+#     push_preview = true,
+# )
 
 makedocs(
     sitename = "TidierIteration.jl"
+    ,authors = "G. Vituri"
+    ,repo = "https://github.com/TidierOrg/TidierIteration.jl"
+    ,format = fmt
     ,pages = [
         "index.md"
         ,"Mapping" => "maps.md"
@@ -15,7 +42,11 @@ makedocs(
     )
 
 deploydocs(
-    repo = "github.com/TidierOrg/TidierIteration.jl.git",
+    repo = "github.com/TidierOrg/TidierIteration.jl.git"
+    ,target="build"
+    ,branch = "gh-pages"
+    ,devbranch = "main"
+    ,push_preview = true
 )
 
 # makedocs(;
